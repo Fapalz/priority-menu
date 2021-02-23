2ac8665f6cfeee9b23280005c0b1a7ef358e8441
Fapalz <blacesmot@gmail.com>
2021-02-23 18:49:49 +0500

    build: playground in eslintignore

---
+++ playground/main.js
@@ -0,0 +1,6 @@
+import '../build/index.css'
+import PriorityMenu from '../build/index.esm.browser'
+
+const nav = new PriorityMenu('.page-navigation', {
+  itemsListSelector: 'ul',
+})
--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  name: 'MyLibrary',
+  name: 'PriorityMenu',
   outputName: 'index',
   external: [],
 }
